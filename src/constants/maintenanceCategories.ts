38036076762a1fd681fbe47776993c7aba994690
Thiruppugazh Srinivasan <thiruppugazhs@gmail.com>
2026-08-14 15:32:08 +0000

    constants(maintenance): define 17 standard maintenance categories

---
+++ src/constants/maintenanceCategories.ts
@@ -0,0 +1,19 @@
+export const STANDARD_MAINTENANCE_CATEGORIES = [
+  "Engine Oil",
+  "Oil Filter",
+  "Air Filter",
+  "Fuel Filter",
+  "Brake Service",
+  "Brake Pad Replacement",
+  "Tyre Rotation",
+  "Tyre Replacement",
+  "Battery",
+  "AC Service",
+  "Coolant",
+  "Transmission",
+  "Suspension",
+  "Wheel Alignment",
+  "General Service",
+  "Inspection",
+  "Other"
+] as const;
